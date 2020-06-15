6248bbc2a88277a5e4eaaced76867fd7f9dc3006
Alicia-Sheng <duowensheng@gmail.com>
2020-06-15 23:43:38 +0800

    Initialize files

--- App.js
+++ App.js
@@ -1,62 +1,19 @@
 import React from 'react';
-import { Platform } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
-import {
-  createAppContainer,
-} from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
-import { createBottomTabNavigator } from 'react-navigation-tabs';
-import Home from './Screens/Home';
-import Detail from './Screens/Detail';
-import Settings from './Screens/Settings';
+import { StyleSheet, Text, View } from 'react-native';
 
-const HomeStack = createStackNavigator({
-  Home: {
-    screen: Home,
-    navigationOptions: { title: 'Home' },
-  },
-  Detail: {
-    screen: Detail,
-    navigationOptions: { title: 'Detail' },
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
-
-const SettingsStack = createStackNavigator({
-  Settings: {
-    screen: Settings,
-    navigationOptions: { title: 'Settings' },
-  },
-});
-
-const AppNavigator = createBottomTabNavigator(
-  {
-    Home: HomeStack,
-    Settings: SettingsStack,
-  },
-  {
-    initialRouteName: 'Home',
-    defaultNavigationOptions: ({ navigation }) => ({
-      // tabBarIcon: () => {
-      tabBarIcon: ({ tintColor }) => {
-        const { routeName } = navigation.state;
-
-        let iconName;
-        if (routeName === 'Home') {
-          //iconName = `ios-home`;
-          iconName = `${Platform.OS === 'ios' ? 'ios' : 'md'}-home`;
-        } else if (routeName === 'Settings') {
-          //iconName = `ios-settings`;
-          iconName = `${Platform.OS === 'ios' ? 'ios' : 'md'}-settings`;
-        }
-
-        return <Ionicons name={iconName} size={20} color={tintColor} />;
-      },
-      tabBarOptions: {
-        activeTintColor: 'blue',
-        inactiveTintColor: '#556',
-      },
-    }),
-  },
-);
-
-export default createAppContainer(AppNavigator);
