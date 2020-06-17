7d7553115f3aa56ca9c1adb038d933df1bf4b934
MRSA-J <mrsa@brandeis.edu>
2020-06-17 20:47:34 +0800

    add a logog to the Account navigation page

--- Screens/Account.js
+++ Screens/Account.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Button, StyleSheet, Text, View, TouchableOpacity, Alert } from 'react-native';
+import { Button, StyleSheet, Text, View, TouchableOpacity, Alert, Image } from 'react-native';
 import Profile from './Profile'
 
 const Settings = ({ navigation }) => {
   return (
     <View style={styles.container}>
+      <Image style = {styles.logo} source = {require('../media/logo/foodie.png')} />
 
       {/* Link to Profile */}
       <TouchableOpacity onPress={() => { navigation.navigate('Profile') }} style={styles.button}>
@@ -35,6 +36,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems:"center",
     height:1000,
+    justifyContent: "center",
+
   },
   button: {
     width: 300,
@@ -44,12 +47,15 @@ const styles = StyleSheet.create({
     backgroundColor: "#DDDDDD",
     padding: 10,
     borderWidth: 1,
-    borderColor: "#00CCFF",
+    borderColor: "#FFFFFF",
   },
   buttonText: {
     alignItems: "center",
     padding: 10,
-
+  },
+  logo: {
+    height: 100,
+    width: 100,
   },
 });
 
