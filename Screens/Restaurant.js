d2b163d5722a8380f5e8511e988ca101bddd0c68
Alicia-Sheng <duowensheng@gmail.com>
2020-06-18 22:05:29 +0800

    Update stylesheet

--- Screens/Restaurant.js
+++ Screens/Restaurant.js
@@ -4,17 +4,17 @@ import { Button, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 const Restaurant = ({ navigation }) => (
     <View style={styles.container}>
         <TouchableOpacity onPress={() => navigation.navigate('Sherman')}>
-          <View style={{margin: 10, alignItems: 'center', width: 300, padding: 10, backgroundColor: '#ccc', borderColor: 'white', borderWidth: 1, borderRadius:15}} >
+          <View style={styles.touch} >
             <Text>Sherman Dining Hall</Text>
           </View>
         </TouchableOpacity>
         <TouchableOpacity onPress={() => navigation.navigate('Stein')}>
-          <View style={{margin: 10, alignItems: 'center', width: 300, padding: 10, backgroundColor: '#ccc', borderColor: 'white', borderWidth: 1, borderRadius:15}} >
+          <View style={styles.touch} >
             <Text>The Stein</Text>
           </View>
         </TouchableOpacity>
         <TouchableOpacity onPress={() => navigation.navigate('Dunkin')}>
-          <View style={{margin: 10, alignItems: 'center', width: 300, padding: 10, backgroundColor: '#ccc', borderColor: 'white', borderWidth: 1, borderRadius:15}} >
+          <View style={styles.touch} >
             <Text>Dunkin Donuts</Text>
           </View>
         </TouchableOpacity>
@@ -27,6 +27,16 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
+    },
+    touch: {
+        margin: 10,
+        alignItems: 'center',
+        width: 300,
+        padding: 10,
+        backgroundColor: '#ccc',
+        borderColor: 'white',
+        borderWidth: 1,
+        borderRadius:15,
     },
 });
 export default Restaurant;
