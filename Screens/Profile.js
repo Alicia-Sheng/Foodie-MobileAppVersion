370a75c666622afe7ab53ee015c50a2ba4c42889
Wenxuan Jiang <wenxuanjiang@brandeis.edu>
2020-06-18 01:21:36 -0400

    Delete useless codes

--- Screens/Profile.js
+++ Screens/Profile.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ImageBackground, ScrollView, StyleSheet, Text, View, Alert, Image } from 'react-native';
+import { ScrollView, StyleSheet, View, Image } from 'react-native';
 import { Icon, ListItem } from 'react-native-elements'
 import user from '../assets/userInfo'
 
@@ -25,7 +25,7 @@ const Profile = ({ navigation }) => (
       {/* Background pic */}
       <ListItem
         title="Background"
-        // onPress={() => { navigation.navigate('Profile') }}
+        // onPress={() => { () => Alert.alert('Not implemented yet') }}
         containerStyle={styles.listItemContainer}
         rightAvatar={<Image source={user.bcg} style={{ width: 50, height: 50 }} />}
         rightIcon={<Icon
@@ -98,48 +98,8 @@ const Profile = ({ navigation }) => (
 );
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    // justifyContent: 'center',
-  },
-  img: {
-    width: 100,
-    height: 100,
-    alignItems: 'center',
-  },
-  profile: {
-    alignItems: 'center',
-  },
   scroll: {
     backgroundColor: "#FFF",
-  },
-  headContainer: {
-  },
-  headerBackgroundImage: {
-    resizeMode: "cover",
-  },
-  headerColumn: {
-    backgroundColor: 'transparent',
-    alignItems: 'center',
-    marginBottom: 10,
-  },
-  userImage: {
-    borderColor: '#FFF',
-    borderRadius: 85,
-    borderWidth: 3,
-    height: 100,
-    marginTop: 15,
-    marginBottom: 15,
-    width: 100,
-  },
-  userNameText: {
-    color: '#FFF',
-    fontSize: 22,
-    fontWeight: 'bold',
-    paddingBottom: 15,
-    textAlign: 'center',
   },
   listItemContainer: {
     height: 55,
