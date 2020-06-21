6f716e4d7b84ab0bef4d580d1abd3de6714874d4
L Pang <56982522+L-Pang@users.noreply.github.com>
2020-06-20 22:38:10 -0700

    Update checkout

--- AppContainer.js
+++ AppContainer.js
@@ -8,6 +8,7 @@ import Home from './Screens/Home';
 import Detail from './Screens/Detail';
 import Restaurant from './Screens/Restaurant';
 import Order from './Screens/Order';
+import Checkout from './Screens/Checkout';
 import Account from './Screens/Account/Account';
 import Profile from './Screens/Account/Profile';
 import MyOrders from './Screens/Account/MyOrders';
@@ -20,145 +21,142 @@ import Starbucks from './Screens/Menu/Starbucks';
 import Login from './Screens/Login';
 
 const HomeStack = createStackNavigator({
-  Home: {
-    screen: Home,
-    navigationOptions: { title: 'Home' },
-  },
-  Detail: {
-    screen: Detail,
-    navigationOptions: { title: 'Detail' },
-  },
+    Home: {
+        screen: Home,
+        navigationOptions: { title: 'Home' },
+    },
+    Detail: {
+        screen: Detail,
+        navigationOptions: { title: 'Detail' },
+    },
 });
 
 const RestaurantStack = createStackNavigator({
-  Restaurant: {
-    screen: Restaurant,
-    navigationOptions: { title: 'Restaurant' },
-  },
-  Sherman: {
-    screen: Sherman,
-    navigationOptions: { title: 'Sherman Dining Hall' },
-  },
-  Stein: {
-    screen: Stein,
-    navigationOptions: { title: "The Stein" },
-  },
-  Dunkin: {
-    screen: Dunkin,
-    navigationOptions: { title: "Dunkin Donuts" },
-  },
-  Einstein: {
-    screen: Einstein,
-    navigationOptions: { title: "Einstein Bros. Bagels" },
-  },
-  Starbucks: {
-    screen: Starbucks,
-    navigationOptions: { title: "Starbucks Farber" },
-  },
+    Restaurant: {
+        screen: Restaurant,
+        navigationOptions: { title: 'Restaurant' },
+    },
+    Sherman: {
+        screen: Sherman,
+        navigationOptions: { title: 'Sherman Dining Hall' },
+    },
+    Stein: {
+        screen: Stein,
+        navigationOptions: { title: "The Stein" },
+    },
+    Dunkin: {
+        screen: Dunkin,
+        navigationOptions: { title: "Dunkin Donuts" },
+    },
+    Einstein: {
+        screen: Einstein,
+        navigationOptions: { title: "Einstein Bros. Bagels" },
+    },
+    Starbucks: {
+        screen: Starbucks,
+        navigationOptions: { title: "Starbucks Farber" },
+    },
 });
 
 const OrderStack = createStackNavigator({
-  Order: {
-    screen: Order,
-    navigationOptions: { title: 'Order' },
-  },
-  // Checkout: {
-  //   screen: Checkout,
-  //   navigationOptions: { title: 'Checkout' },
-  // },
+    Order: {
+        screen: Order,
+        navigationOptions: { title: 'Order' },
+    },
+    Checkout: {
+        screen: Checkout,
+        navigationOptions: { title: 'Checkout' },
+    },
 });
 
 const AccountStack = createStackNavigator({
-  Account: {
-    screen: Account,
-    navigationOptions: { title: 'Account' },
-  },
-  Profile: {
-    screen: Profile,
-    navigationOptions: { title: 'Profile' },
-  },
-  MyOrders: {
-    screen: MyOrders,
-    navigationOptions: { title: 'My Orders' },
-  },
-  MyReviews: {
-    screen: MyReviews,
-    navigationOptions: { title: 'My Reviews' },
-  },
+    Account: {
+        screen: Account,
+        navigationOptions: { title: 'Account' },
+    },
+    Profile: {
+        screen: Profile,
+        navigationOptions: { title: 'Profile' },
+    },
+    MyOrders: {
+        screen: MyOrders,
+        navigationOptions: { title: 'My Orders' },
+    },
+    MyReviews: {
+        screen: MyReviews,
+        navigationOptions: { title: 'My Reviews' },
+    },
 });
 
-const AppNavigator = createBottomTabNavigator(
-  {
+const AppNavigator = createBottomTabNavigator({
     Home: HomeStack,
     Restaurant: RestaurantStack,
     Order: OrderStack,
     Account: AccountStack
-  },
-  {
+}, {
     initialRouteName: 'Home',
     defaultNavigationOptions: ({ navigation }) => ({
-      // tabBarIcon: () => {
-      tabBarIcon: ({ tintColor }) => {
-        const { routeName } = navigation.state;
+            // tabBarIcon: () => {
+            tabBarIcon: ({ tintColor }) => {
+                const { routeName } = navigation.state;
 
-        let iconName;
-        let badgeCount;
-        if (routeName === 'Home') {
-          //iconName = `ios-home`;
-          iconName = `${Platform.OS === 'ios' ? 'ios' : 'md'}-home`;
-        } else if (routeName === 'Restaurant') {
-          //iconName = `ios-restaurant`;
-          iconName = `${Platform.OS === 'ios' ? 'ios' : 'md'}-restaurant`;
-        } else if (routeName === 'Order') {
-          //iconName = `ios-cart`;
-          iconName = `${Platform.OS === 'ios' ? 'ios' : 'md'}-cart`;
-          badgeCount = 0;
-        } else if (routeName === 'Account') {
-          //iconName = `ios-person`;
-          iconName = `${Platform.OS === 'ios' ? 'ios' : 'md'}-person`;
-        }
+                let iconName;
+                let badgeCount;
+                if (routeName === 'Home') {
+                    //iconName = `ios-home`;
+                    iconName = `${Platform.OS === 'ios' ? 'ios' : 'md'}-home`;
+                } else if (routeName === 'Restaurant') {
+                    //iconName = `ios-restaurant`;
+                    iconName = `${Platform.OS === 'ios' ? 'ios' : 'md'}-restaurant`;
+                } else if (routeName === 'Order') {
+                    //iconName = `ios-cart`;
+                    iconName = `${Platform.OS === 'ios' ? 'ios' : 'md'}-cart`;
+                    badgeCount = 0;
+                } else if (routeName === 'Account') {
+                    //iconName = `ios-person`;
+                    iconName = `${Platform.OS === 'ios' ? 'ios' : 'md'}-person`;
+                }
 
-        return (
-          <>
-          <Ionicons name={iconName}
-            size={20}
-            color={tintColor}
-          />
-            {/* {badgeCount > 0 && (
-              <View
-                style={{
-                  position: 'absolute',
-                  right: 25,
-                  top: -1,
-                  backgroundColor: '#cf3838',
-                  borderRadius: 6,
-                  width: 12,
-                  height: 12,
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                }}
-              >
-                <Text style={{ color: 'white', fontSize: 10, fontWeight: 'bold' }}>
-                  {badgeCount}
-                </Text>
-              </View>
-            )} */}
-          </>
-        )
-          ;
-      },
-      tabBarOptions: {
-        activeTintColor: '#cf3838',
-        inactiveTintColor: '#556',
-      },
+                return ( <
+                        >
+                        <
+                        Ionicons name = { iconName }
+                        size = { 20 }
+                        color = { tintColor }
+                        /> {
+                        /* {badgeCount > 0 && (
+                                      <View
+                                        style={{
+                                          position: 'absolute',
+                                          right: 25,
+                                          top: -1,
+                                          backgroundColor: '#cf3838',
+                                          borderRadius: 6,
+                                          width: 12,
+                                          height: 12,
+                                          justifyContent: 'center',
+                                          alignItems: 'center',
+                                        }}
+                                      >
+                                        <Text style={{ color: 'white', fontSize: 10, fontWeight: 'bold' }}>
+                                          {badgeCount}
+                                        </Text>
+                                      </View>
+                                    )} */
+                    } <
+                    />
+            );
+        },
+        tabBarOptions: {
+            activeTintColor: '#cf3838',
+            inactiveTintColor: '#556',
+        },
     }),
-  });
+});
 
-const SwitchNavigator = createSwitchNavigator(
-  {
+const SwitchNavigator = createSwitchNavigator({
     Main: AppNavigator,
     Login
-  }
-);
+});
 
 export default createAppContainer(SwitchNavigator);
--- Screens/Checkout.js
+++ Screens/Checkout.js
@@ -2,14 +2,15 @@ import React from 'react';
 import styled from 'styled-components/native';
 import { Text, View } from 'react-native';
 import { useMutation } from 'react-apollo';
-import Button from '../Button/Button';
-import { COMPLETE_ORDER } from '../../constants';
+import Button from '../Components/Button/Button';
+import { COMPLETE_ORDER } from '../constants';
 
 const CheckoutWrapper = styled(View)`
   display: flex;
   justify-content: space-between;
   flex-direction: column;
   margin: 2% 5%;
+  align-items: center;
 `;
 
 const Checkout = ({ navigation }) => {
@@ -21,10 +22,13 @@ const Checkout = ({ navigation }) => {
                 <Text>Completed checkout!</Text>
             ) : (
                     <>
-                        <p>This is the checkout, press the button below to complete:</p>
-                        <Button color='royalBlue' onPress={completeOrder}>
-                            Complete checkout
-          </Button>
+                        <Text>Press the button below to complete checkout</Text>
+                        <Button
+                            title="Complete checkout"
+                            onPress={completeOrder}
+                            width="70%"
+                            height="40px"
+                        />
                     </>
                 )}
         </CheckoutWrapper>
