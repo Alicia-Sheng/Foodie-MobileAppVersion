f08181cecae53025551e5d1a1830fc17834bd380
L Pang <56982522+L-Pang@users.noreply.github.com>
2020-06-30 15:49:59 -0700

    Create user.js

---
+++ assets/user.js
@@ -0,0 +1,35 @@
+const user = [{
+    "id": 1,
+    "username": "test",
+    "password": "test",
+    "email": "test@brandeis.edu",
+    "phone": "1234567890",
+    "role": "basic"
+},
+{
+    "id": 2,
+    "username": "admin",
+    "password": "admin",
+    "email": "admin@brandeis.edu",
+    "phone": "1234567890",
+    "role": "admin"
+},
+{
+    "id": 3,
+    "username": "Dan",
+    "password": "Dan",
+    "email": "dan@brandeis.edu",
+    "phone": "1234567890",
+    "role": "basic"
+},
+{
+    "id": 4,
+    "username": "Jess",
+    "password": "Jess",
+    "email": "jessica@brandeis.edu",
+    "phone": "1234567890",
+    "role": "basic"
+}
+]
+
+export default user;
