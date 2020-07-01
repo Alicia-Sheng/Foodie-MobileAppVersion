2e073fb9b65820e7ef52e4d1959848053c0ddffa
MRSA-J <mrsa@brandeis.edu>
2020-07-01 17:19:37 +0800

    delete unnecessary code

--- Screens/Account/MyReviews.js
+++ Screens/Account/MyReviews.js
@@ -6,16 +6,6 @@ import user from '../../assets/userInfo';
 import BackButton from '../../Components/Button/BackButton';
 import StarRating from '../../Components/Details/StarRating';
 import { GET_ORDER, GET_REVIEW, GET_CURRENT_USER_REVIEW } from '../../constants/functions';
-{/*
-const Reviews = ({ orders}) => {
-  return orders.map((order, index) => {
-    return (
-      <Review key={index} id={order.userId} />
-    )
-  })
-}
-*/}
-
 
 const Review = () => {
   const { loading, error, data } = useQuery(GET_CURRENT_USER_REVIEW);
@@ -70,88 +60,6 @@ const Comment = ({item}) => {
     );
   })
 }
-
-
-
-
-
-// const Reviews = ({ reviews }) => {
-//   //return user.reviews.map((review) => {
-//   return reviews.map((review) => {
-//     return (
-//       <TouchableOpacity
-//         key={review.comment}
-//         onPress={() => Alert.alert('Not implemented yet')}
-//         style={styles.listItemContainer}
-//       >
-//         <View style={styles.title}>
-//           <Text style={{ fontWeight: "bold" }}>
-//             {review.id}
-//           </Text>
-//           <Text style={{ marginHorizontal: 5 }}>
-//             at
-//           </Text>
-//           {// <Text style={{ fontStyle: "italic" }}>
-//           //   {review.location}
-//           // </Text>
-//           }
-//         </View>
-//         <View style={styles.text}>
-//           <Text>
-//             {review.comment}
-//           </Text>
-//         </View>
-//       </TouchableOpacity>
-//     )
-//   })
-// }
-
-{/*
-function MyReviews() {
-    const { loading, error, data } = useQuery(GET_ORDER);
-    return (
-      <>
-            {(loading || error) ? (
-                <>
-                <Text>{loading ? 'Loading...' : error.message}</Text>
-                </>
-            ):
-            (
-              <>
-                <ScrollView style={styles.scroll}>
-                  <View >
-                    <Reviews orders={data.orders}/>
-                  </View>
-                </ScrollView>
-              </>
-            )}
-      </>
-    )
-}
-
-*/}
-
-
-
-// const MyReviews = ({ navigation }) => {
-// class MyReviews extends Component {
-//
-//   static navigationOptions = () => {
-//     return {
-//       headerLeft: () => <BackButton />
-//     };
-//   };
-//
-//   render() {
-//     return (
-//       <ScrollView style={styles.scroll}>
-//         <View style={styles.headerContainer}>
-//           <Reviews />
-//         </View>
-//       </ScrollView>
-//     )
-//   }
-// }
 
 const styles = StyleSheet.create({
   title: {
